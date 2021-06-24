73d50019585c2dcb3ba48de6e574f58b64ee9a3c
niu <194565688@qq.com>
2021-06-24 10:56:47 +0800

    第一次提交

---
+++ day35_javase03/src/test/java/Hi.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+/**
+ * @author shkstart
+ * @create 2021-05-25 23:27
+ */
+public class Hi {
+    @Test
+    public void tt(){
+        System.out.println("我是人");
+    }
+}
